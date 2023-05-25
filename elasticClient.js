2058ba214c49bc4aa4819db7e4901584e9e6366b
Aakash <aakashmangla15@gmail.com>
2023-05-25 15:34:28 +0530

    Add elasticsearch client

--- elasticClient.js
+++ elasticClient.js
@@ -1,6 +1,6 @@
 const { Client } = require("@elastic/elasticsearch");
 const fs = require("fs")
-require("dotenv").config({ path: ".elastic.env" });
+require("dotenv").config();
 
 const elasticClient = new Client({
     node: 'https://localhost:9200',
@@ -8,7 +8,7 @@ const elasticClient = new Client({
         username: process.env.ELASTIC_USERNAME,
         password: process.env.ELASTIC_PASSWORD,
     },
-    caFingerprint: 'E5:35:DA:62:6A:88:72:BE:E2:BF:0A:AB:9B:B1:99:BE:88:71:07:1B:BD:65:92:12:13:5A:8A:86:88:09:F6:03',
+    log: 'trace',
     tls: {
         ca: fs.readFileSync('./http_ca.crt'),
         rejectUnauthorized: false
